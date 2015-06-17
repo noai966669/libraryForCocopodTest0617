176ba343310e80cb505a467585b4ccaad88060ab
ai966669 <ai966669@ai966669.local>
2015-06-17 19:15:31 +0800

    first commit

---
+++ libraryForCocopodTest0617/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  libraryForCocopodTest0617
+//
+//  Created by ai966669 on 15/6/17.
+//  Copyright (c) 2015年 ai966669. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ libraryForCocopodTest0617/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  libraryForCocopodTest0617
+//
+//  Created by ai966669 on 15/6/17.
+//  Copyright (c) 2015年 ai966669. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
